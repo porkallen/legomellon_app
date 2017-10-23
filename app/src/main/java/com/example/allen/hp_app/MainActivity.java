8d9178f21885e06987d3481825d391dbec39d513
Nanica <liulanm518@outlook.com>
2017-10-22 22:24:21 -0700

    Fix the crunching bug. Fix the 9-pitches pic bug.

--- app/src/main/java/com/example/allen/hp_app/MainActivity.java
+++ app/src/main/java/com/example/allen/hp_app/MainActivity.java
@@ -1,34 +1,62 @@
 package com.example.allen.hp_app;
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.widget.EditText;
-import android.widget.Scroller;
-import android.widget.TextView;
-import android.widget.TextView.BufferType;
 
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Lan on 10/22/17.
+ */
 
 public class MainActivity extends AppCompatActivity {
+
+    private ListView msgListView;
+    private EditText input;
+    private Button sent;
+    private MsgAdapter msgAdapter;
+
+
+    private List<Msg> msgList = new ArrayList<Msg>();
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+
+    protected void onCreate (Bundle savedInstanceState){
+
         super.onCreate(savedInstanceState);
+
+
         setContentView(R.layout.conversation_main);
-        // Get the widgets reference from XML layout
-        final EditText dumpText = (EditText) findViewById(R.id.input_text);
-        /*
-            This will provide Scroll option for EditText,
-            but it will not show the scroll bar on EditText
-        */
-        dumpText.setText("Test 1",TextView.BufferType.EDITABLE);
-        dumpText.setScroller(new Scroller(getApplicationContext()));
-        dumpText.setVerticalScrollBarEnabled(true);
-        /*
-            setMinLines (int minlines)
-                Makes the TextView at least this many lines tall.
-                Setting this value overrides any other (minimum) height
-                setting. A single line TextView will set this value to 1.
-         */
-        // Set the minimum lines to display on EditText
-        dumpText.setMinLines(2);
+
+        msgAdapter = new MsgAdapter( MainActivity.this, R.layout.msg_item, msgList);
+        input = (EditText)findViewById(R.id.input_text);
+        sent = (Button) findViewById(R.id.send);
+        msgListView = (ListView) findViewById(R.id.msg_list_view);
+        msgListView.setAdapter(msgAdapter);
+
+        sent.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View view){
+                String msgContent = input.getText().toString();
+                if (!"".equals(msgContent)){
+                    Msg msg = new Msg(msgContent, Msg.TypeSent);
+                    msgList.add(msg);
+
+                    msgAdapter.notifyDataSetChanged();
+                    msgListView.setSelection(msgList.size());
+                    input.setText("");
+                }
+            }
+
+        });
 
     }
+
+
 }
